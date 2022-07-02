7310b315594e0e2b5376fed3a4c0956cc615a541
Alex Volkov <alex@flamy.ca>
2022-07-02 15:26:51 -0400

    Updating metadata for test run

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ plugin_package = "octoprint_ublmeshedit"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-UBLMeshEdit"
+plugin_name = "OctoPrint-UBLMeshEdit test config"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.2"
+plugin_version = "0.3.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -27,7 +27,7 @@ plugin_author = "Taylor Talkington"
 plugin_author_email = "taylor.talkington@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/The-EG/OctoPrint-UBLMeshEdit"
+plugin_url = "https://github.com/avolkov/OctoPrint-UBLMeshEdit/tree/rect-mesh"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
